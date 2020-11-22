b5ff15acdd416d71c7c4b3e8e92a6f2c7d06f3bb
eug3nio <eugenio.merlin@hotmail.com>
2020-11-22 00:11:17 -0300

    Correção na migration

--- src/main/resources/db/migration/V07__insert_categoria_residuos_solicitacao.sql
+++ src/main/resources/db/migration/V07__insert_categoria_residuos_solicitacao.sql
@@ -1,15 +1,15 @@
 --Insert de Categorias
 
 
-INSERT INTO `edestiny`.`categoria` (`descricao`,`ativo`)
+INSERT INTO categoria (descricao,ativo)
 VALUES
 ('Smartphone', '1');
 
-INSERT INTO `edestiny`.`categoria`(`descricao`,`ativo`)
+INSERT INTO categoria(descricao,ativo)
 VALUES
 ('Computadores', '1');
 
-INSERT INTO `edestiny`.`categoria`(`descricao`,`ativo`)
+INSERT INTO categoria(descricao,ativo)
 VALUES
 ( 'Tablet','1');
 
@@ -29,54 +29,54 @@ VALUES ('Rua Crescência Bertoldi', '100', '', 'Rua Crescência Bertoldi', '8149
 
 --Insert de Detentor
 
-INSERT INTO `edestiny`.`detentor`(`nome`,`email`,`senha`,`telefone`,`foto_perfil`, endereco_id)
+INSERT INTO detentor(nome,email,senha,telefone,foto_perfil, endereco_id)
 VALUES
 ('Ewerton','ewerton.cap@hotmail.com','123','996495906', null, (SELECT id FROM endereco WHERE cep = '81070474'));
 
-INSERT INTO `edestiny`.`detentor`(`nome`,`email`,`senha`,`telefone`,`foto_perfil`, endereco_id)
+INSERT INTO detentor(nome,email,senha,telefone,foto_perfil, endereco_id)
 VALUES
 ('Eugenio','eugenio@hotmail.com','123','33333333',null, (SELECT id FROM endereco WHERE cep = '82650312'));
 
-INSERT INTO `edestiny`.`detentor`(`nome`,`email`,`senha`,`telefone`,`foto_perfil`, endereco_id)
+INSERT INTO detentor(nome,email,senha,telefone,foto_perfil, endereco_id)
 VALUES
 ('Gustavo','gustavo@hotmail.com','123','9999999',null, (SELECT id FROM endereco WHERE cep = '82650455'));
 
-INSERT INTO `edestiny`.`detentor`(`nome`,`email`,`senha`,`telefone`,`foto_perfil`, endereco_id)
+INSERT INTO detentor(nome,email,senha,telefone,foto_perfil, endereco_id)
 VALUES
 ('Pedro','pedro@hotmail.com','123','11111111',null, (SELECT id FROM endereco WHERE cep = '81490476'));
 
 
 --Inseert de Solicitações
 
-INSERT INTO `edestiny`.`solicitacao`(`solicitante_id`,`situacao`,`coleta_id`,`justificativa`,`dt_solicitacao`)
+INSERT INTO solicitacao(solicitante_id,situacao,coleta_id,justificativa,dt_solicitacao)
 VALUES
 ('1','ABERTA',null,null,'2020-11-20');
 
-INSERT INTO `edestiny`.`solicitacao`(`solicitante_id`,`situacao`,`coleta_id`,`justificativa`,`dt_solicitacao`)
+INSERT INTO solicitacao(solicitante_id,situacao,coleta_id,justificativa,dt_solicitacao)
 VALUES
 ('2','ABERTA',null,null,'2020-11-21');
 
 
-INSERT INTO `edestiny`.`solicitacao`(`solicitante_id`,`situacao`,`coleta_id`,`justificativa`,`dt_solicitacao`)
+INSERT INTO solicitacao(solicitante_id,situacao,coleta_id,justificativa,dt_solicitacao)
 VALUES
 ('3','ABERTA',null,null,'2020-11-22');
 
-INSERT INTO `edestiny`.`solicitacao`(`solicitante_id`,`situacao`,`coleta_id`,`justificativa`,`dt_solicitacao`)
+INSERT INTO solicitacao(solicitante_id,situacao,coleta_id,justificativa,dt_solicitacao)
 VALUES
 ('4','ABERTA',null,null,'2020-11-23');
 SELECT * FROM edestiny.solicitacao;
 
 --Insert de Residuos
 
-INSERT INTO `edestiny`.`residuo`(`descricao`,`quantidade`,`unidade_medida`,`categoria_id`,`solicitacao_id`)
+INSERT INTO residuo(descricao,quantidade,unidade_medida,categoria_id,solicitacao_id)
 VALUES
 ('Celular','1','QUANTIDADE','2','1');
 
-INSERT INTO `edestiny`.`residuo`(`descricao`,`quantidade`,`unidade_medida`,`categoria_id`,`solicitacao_id`)
+INSERT INTO residuo(descricao,quantidade,unidade_medida,categoria_id,solicitacao_id)
 VALUES
 ('Tabet','1','QUANTIDADE','2','2');
 
-INSERT INTO `edestiny`.`residuo`(`descricao`,`quantidade`,`unidade_medida`,`categoria_id`,`solicitacao_id`)
+INSERT INTO residuo(descricao,quantidade,unidade_medida,categoria_id,solicitacao_id)
 VALUES
 ('Notebook','1','KG','2','3');
 
